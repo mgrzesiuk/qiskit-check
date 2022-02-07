a57e0411daf1c050a96fdb3ed6e690c5bcd354f6
mgrzesiuk <marekgrzesiuk99@gmail.com>
2022-02-07 10:30:33 +0000

    add hopf coordinates to state vector conversion

--- qiskit_check/property_test/utils.py
+++ qiskit_check/property_test/utils.py
@@ -1,10 +1,25 @@
+from cmath import polar, cos, exp, sin
+from math import acos
 from typing import Tuple
 
 
 def vector_state_to_hopf_coordinates(
         ground_state_amplitude: complex, excited_state_amplitude: complex) -> Tuple[float, float]:
-    pass
+    """
+    :param ground_state_amplitude:
+    :param excited_state_amplitude:
+    :return:
+    """
+    ground_state_abs, ground_state_phase = polar(ground_state_amplitude)
+    _, excited_state_phase = polar(excited_state_amplitude)
+
+    theta = 2 * acos(ground_state_abs)
+    relative_phase = excited_state_phase - ground_state_phase
+
+    return theta, relative_phase
 
 
 def hopf_coordinates_to_vector_state(angle: float, relative_phase: float) -> Tuple[complex, complex]:
-    pass
+    ground_state_amp = cos(angle/2)
+    excited_state_amp = exp(relative_phase*1j) * sin(angle/2)
+    return ground_state_amp, excited_state_amp
