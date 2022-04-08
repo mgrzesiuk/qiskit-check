f06df70750eb58b685825aa403c58b5675dcbe75
puma3141 <marekgrzesiuk99@gmail.com>
2022-04-08 05:19:25 +0200

    setup, new config, and final fixes before submit

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
 
 setup(
@@ -10,13 +9,18 @@ setup(
     version='1.1.1',
     license='MIT',
     author="Marek Grzesiuk",
-    packages=['qiskit_check'],
+    packages=find_packages(include=['qiskit_check', 'qiskit_check.*']),
     url='https://github.com/mgrzesiuk/qiskit_check',
     keywords='property-based-testing qiskit quantum-computing',
     install_requires=[
-          'qiskit>=0.34.2',
-      ],
+          'qiskit==0.34.2',
+          'numpy==1.21.2',
+          'scipy==1.7.2',
+          'pyyaml==6.0',
+          'colorama==0.4.4',
+          'qiskit-utils==1.1.5',
+    ],
     description="tool for property based testing in qiskit",
-    long_description=long_description,
+    long_description="library for property based testing of quantum programs written in qiskit",
     long_description_content_type='text/markdown'
 )
